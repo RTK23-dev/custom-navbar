94c9866d1f38cb65816a0e8d228eab99359b4ff3
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-07-15 16:36:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@ n stable
 PATH="$PATH"
 npm i -g yarn
 cd /var/www/pterodactyl
-php artisan down
 yarn install
 yarn add @emotion/react
 yarn add @fortawesome/free-brands-svg-icons
 rm /var/www/pterodactyl/resources/scripts/NavigationBar.tsx
 cd /var/www/pterodactyl/resources/scripts
 wget https://raw.githubusercontent.com/TwixGamer00/vloxCUSTOMS/main/NavigationBar.tsx
+echo 'Please wait while the panel is rebuilding'
 yarn run build:production
-php artisan up
