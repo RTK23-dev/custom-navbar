6b56b7e90bc46a4be6d7950e9f3918d89fb5f8cf
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-07-15 18:40:18 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+yarn add @fortawesome/free-brands-svg-icons
+rm /var/www/pterodactyl/resources/scripts/components/NavigationBar.tsx
+cd /var/www/pterodactyl/resources/scripts/components
+wget https://raw.githubusercontent.com/TwixGamer00/vloxCUSTOMS/main/NavigationBar.tsx
+cd /var/www/pterodactyl
+echo 'Please wait while the panel is rebuilding'
+yarn run build:production
