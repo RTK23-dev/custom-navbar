0650a8eb7faef66bfd8b7440d22a013ec3d1f94d
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-08-16 17:06:13 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,7 +1,7 @@
 yarn add @fortawesome/free-brands-svg-icons
 rm /var/www/pterodactyl/resources/scripts/components/NavigationBar.tsx
 cd /var/www/pterodactyl/resources/scripts/components
-wget https://raw.githubusercontent.com/RTK23-DEV/vloxCUSTOMS/main/NavigationBar.tsx
+wget https://raw.githubusercontent.com/RTK23-DEV/vloxCUSTOMS/dark/NavigationBar.tsx
 cd /var/www/pterodactyl
 echo 'Please wait while the panel is rebuilding'
 yarn run build:production
