b61a5fe1caf873fa50be826e23caf46e395978f1
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-10-02 17:31:51 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,6 +1,4 @@
 yarn add @fortawesome/free-brands-svg-icons
-rm /var/www/pterodactyl/resources/scripts/components/NavigationBar.tsx
-cd /var/www/pterodactyl/resources/scripts/components
 cd /var/www/pterodactyl
 echo 'Please wait while the panel is rebuilding'
 yarn run build:production
